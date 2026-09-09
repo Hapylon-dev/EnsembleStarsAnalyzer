0dc11d869b3cdb5c08b01b5a5fdbead2fd001e08
Koufuku <koufuku.524.916@gmail.com>
2026-09-09 19:59:08 +0900

    Disable debug mode for production

--- src/config.py
+++ src/config.py
@@ -14,7 +14,7 @@ from dataclasses import dataclass
 APP_NAME = "Ensemble Stars!! Music Judgement Analyzer"
 VERSION = "1.3 Final"
 
-DEBUG_MODE = True
+DEBUG_MODE = False
 
 # ==========================================================
 # YOLO Graph Detector
